fc0f0f41666ed225cb7460dbc8172535940c673c
Franscesco Romeo <romeofrancesco@yahoo.com>
2021-02-01 20:53:02 +0100

    upload all

--- main_HW3.R
+++ main_HW3.R
@@ -2,6 +2,8 @@
 # Point 1 -----------------------------------------------------------------
 
 load("../Connect-your-Brain/data/hw3_data.RData") # import data
+library(ggraph)
+library(igraph)
 
 asd_sel # autistc brain list
 td_sel # tipiccaly developed brain list
@@ -35,9 +37,43 @@ td_bind = do.call(rbind, td_sel)
 asd_bind_cor = cor(asd_bind)
 td_bind_cor = cor(td_bind)
 
-asd_td_bin_matrix = rbind(asd_bind_cor, td_bind_cor) 
+asd_td_bind_matrix = rbind(asd_bind_cor, td_bind_cor) 
+
+t_bind <- quantile(abs(asd_td_bind_matrix), probs = c(0.8)) 
+t_bind_zfisher = zfisher(t_bind)
 
 
+CI_function <- function(N,rho,alpha,t){
+  se = 1/sqrt(N-3)
+  lower = rho - qnorm(1-(alpha/2)/choose(116,2))*se
+  upper = rho + qnorm(1-(alpha/2)/choose(116,2))*se
+  print(c(lower,upper))
+  if (-t>= lower && -t<= upper) return(0)
+  if (t>= lower && t<= upper) return(0)
+  else return(1)
+}
+
+CI_function(34,-0.775,1-0.95,t_bind_zfisher)
+
+
+
+
+
+
+asd_bind_matrix_zfisher = abs(zfisher(asd_bind_cor))
+asd_bind_matrix_zfisher[asd_bind_matrix_zfisher>t_bind_zfisher] <- 1
+asd_bind_matrix_zfisher[asd_bind_matrix_zfisher<=t_bind_zfisher] <- 0
+
+td_bind_matrix_zfisher = abs(zfisher(td_bind_cor))
+td_bind_matrix_zfisher[td_bind_matrix_zfisher>t_bind_zfisher] <- 1
+td_bind_matrix_zfisher[td_bind_matrix_zfisher<=t_bind_zfisher] <- 0
+
+
+graph_asd <- graph_from_adjacency_matrix(asd_bind_matrix_zfisher)
+graph_td <- graph_from_adjacency_matrix(td_bind_matrix_zfisher)
+
+write_graph(graph_asd, "graph_asd.txt", format = c("edgelist"))
+write_graph(graph_td, "graph_td.txt", format = c("edgelist"))
 
 # APPRACH 2:
 asd_cor = lapply(asd_sel, cor) # list of correlation matrices
